1972b256b3c9e96b65bdc012e4d19ad994575250
hanepjiv <hanepjiv@gmail.com>
2023-10-07 12:15:30 +0900

    Fixes: use local variables.

--- Module.cmake
+++ Module.cmake
@@ -16,11 +16,14 @@ find_program(SPLINT splint)
 if(NOT SPLINT)
   message(FATAL_ERROR "Test feature requires splint.")
 endif()
+
+set(src "${CMAKE_SOURCE_DIR}/../et-splint/splint.sh.in")
+set(target "${CMAKE_BINARY_DIR}/dependencies/et-splint/splint.sh")
+
 message(STATUS "${SPLINT}")
-configure_file(${abs_top_srcdir}/../dependencies/et-splint/splint.sh.in
-  ${CMAKE_CURRENT_BINARY_DIR}/dependencies/et-splint/splint.sh @ONLY)
+configure_file(${src} ${target} @ONLY)
 set(SPLINT_FLAGS "")
 foreach(loop_var IN LISTS ET_SPLINT_INCLUDE_DIRS)
   set(SPLINT_FLAGS "${SPLINT_FLAGS} -I ${loop_var}")
 endforeach()
-set(SPLINT_SH /bin/env sh ${CMAKE_CURRENT_BINARY_DIR}/dependencies/et-splint/splint.sh)
+set(SPLINT_SH /bin/env sh ${target})
